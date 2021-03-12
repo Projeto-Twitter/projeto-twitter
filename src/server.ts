078b8a2b4b3dca5b2efc1ecd221d17635c21d72a
savio-84 <saviocarvalho01@hotmail.com>
2021-03-11 21:52:55 -0300

    [savio.carvalho] lidando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 require('dotenv').config();
 
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import 'express-async-errors';
 import router from './routes/index';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(router);
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(process.env.PORT, ()=>{
   console.log(`Server started at ${process.env.PORT} door`);
