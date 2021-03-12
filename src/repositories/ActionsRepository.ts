2b278e74f8f6a9684195fed50a80b4719c012a6c
savio-84 <saviocarvalho01@hotmail.com>
2021-03-11 22:04:48 -0300

    [savio.carvalho] deleted unused repositories files

--- src/repositories/ActionsRepository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import Action from '../models/Action'
-
-@EntityRepository(Action)
-class ActionsRepository extends Repository<Action> {}
-
-export default ActionsRepository;
