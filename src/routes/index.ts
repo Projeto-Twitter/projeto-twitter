9876fa236aec92d77abc3165c13783fba78c1868
savio-84 <saviocarvalho01@hotmail.com>
2021-03-17 03:52:31 -0300

    created welcome message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,10 @@ import commentsRouter from './comments.routes';
 
 const router = express.Router();
 
+router.get('/',(request, response) => {
+  return response.status(200).send('Welcome to our fake twitter server')
+});
+
 router.use('/users', usersRouter);
 router.use('/tweets', tweetsRouter);
 router.use('/comments', commentsRouter);
