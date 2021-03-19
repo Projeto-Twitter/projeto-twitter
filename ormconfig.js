ed89ec1e3f3f174ec998556666b47899fae40db9
savio-84 <saviocarvalho01@hotmail.com>
2021-03-19 10:26:36 -0300

    .

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   "port": process.env.DATABASE_PORT,
   "username": process.env.DATABASE_USER,
   "password": process.env.DATABASE_PASSWORD,
-  "database": process.env.DATABASE_URL,
+  "database": process.env.DATABASE_DATABASE,
   "ssl": true,
   "extra": {
     "ssl": {
